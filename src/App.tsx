e949f3e7dd3e95c73c741e2297716f977a2758c7
Piturnah <peterhebden6@gmail.com>
2022-02-27 19:12:52 +0000

    Initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.scss";
+
+interface SetupFormProps {
+  callback: () => void;
+}
+
+const SetupForm = ({ callback }: SetupFormProps) => {
+  const [handed, setHanded] = useState(0);
+
+  const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    callback();
+  };
+
+  useEffect(() => {
+    setHanded(9);
+  }, []);
+
+  return (
+    <div className="setup-form">
+      <h1>Tournament Setup</h1>
+      <form onSubmit={submitForm} className={"is-flex-column"}>
+        <div>
+          <label> Max hands per table: </label>
+          <input
+            type="number"
+            value={handed}
+            onChange={(e) => setHanded(parseInt(e.target.value))}
+          />
+        </div>
+        <label> Players, separated by newlines: </label>
+        <textarea />
+        <button>Start Tournament</button>
+      </form>
+    </div>
+  );
+};
+
+interface TableManagerProps {
+  initialPlayers: string[];
+}
+
+const TableManager = ({ initialPlayers }: TableManagerProps) => (
+  <>Table Manager will go here</>
+);
 
 function App() {
+  enum State {
+    SETUP,
+    RUNNING,
+  }
+
+  const [state, setState] = useState(State.SETUP);
+
+  const start = () => {
+    setState(State.RUNNING);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {state === State.SETUP ? (
+        <SetupForm callback={start} />
+      ) : (
+        <TableManager initialPlayers={[]} />
+      )}
     </div>
   );
 }
